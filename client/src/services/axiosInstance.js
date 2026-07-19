7fd04e70fd73607af78999b9ff3b4ea8fa356b50
pancholipankaj3008 <pancholipankaj3008@gmail.com>
2026-07-20 00:13:33 +0530

    connect backend to frontend

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://ai-powered-resume-builder-0v90.onrender.com/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
