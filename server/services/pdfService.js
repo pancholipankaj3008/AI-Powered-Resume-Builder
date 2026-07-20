59dc3d86e8d03f4fca17c5c7301d25a12ddcd326
pancholipankaj3008 <pancholipankaj3008@gmail.com>
2026-07-20 09:02:16 +0530

    install puppeteer-core

--- server/services/pdfService.js
+++ server/services/pdfService.js
@@ -1,19 +1,14 @@
-const puppeteer = require("puppeteer");
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
 const generateResumePDF = async (resumeId, cookies = {}) => {
 
-    const executablePath = await puppeteer.executablePath();
-
-console.log("Puppeteer executable:", executablePath);
-
-const browser = await puppeteer.launch({
-  executablePath,
-  headless: true,
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-  ],
-});
+    const browser = await puppeteer.launch({
+        executablePath: await chromium.executablePath(),
+        headless: true,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+    });
 
     const page = await browser.newPage();
 
@@ -39,22 +34,22 @@ const browser = await puppeteer.launch({
 
     await page.emulateMediaType("screen");
 
-await page.setViewport({
-    width: 794,
-    height: 1123,
-    deviceScaleFactor: 2,
-});
+    await page.setViewport({
+        width: 794,
+        height: 1123,
+        deviceScaleFactor: 2,
+    });
 
-const pdf = await page.pdf({
-    format: "A4",
-    printBackground: true,
-    margin: {
-        top: "0mm",
-        right: "0mm",
-        bottom: "0mm",
-        left: "0mm",
-    },
-});
+    const pdf = await page.pdf({
+        format: "A4",
+        printBackground: true,
+        margin: {
+            top: "0mm",
+            right: "0mm",
+            bottom: "0mm",
+            left: "0mm",
+        },
+    });
 
     await browser.close();
 
