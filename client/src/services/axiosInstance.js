8dec10ba1f8c2f8fe1bf59bf23b05455d35946a4
Pankaj Pancholi <pancholipankaj3008@gmail.com>
2026-07-21 11:18:49 +0530

    Update axiosInstance.js

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://ai-powered-resume-builder-0v90.onrender.com/",
+    baseURL: "https://ai-powered-resume-builder-n8ic.onrender.com/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
